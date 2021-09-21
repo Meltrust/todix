df938fc23d19b0b9f972a298e4bff3bd500f9ac1
meltrust <original.mtapia@outlook.com>
2021-09-21 01:16:32 -0500

    Add tasks main object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const tasks = [
+  {
+    description: 'Excersice',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Yoga',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Walk the dog',
+    completed: false,
+    index: 3,
+  },
+];
