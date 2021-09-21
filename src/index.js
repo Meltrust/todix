bed40ed65902ba89f2f4ad0fba65b2813ab390f6
meltrust <original.mtapia@outlook.com>
2021-09-21 01:17:29 -0500

    Add iterative displaytasks function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import './style.css';
 
+function displayTasks(tasks) {
+  tasks.forEach((task) => {
+    const mainList = document.getElementById('mainList');
+    const row = document.createElement('li');
+    const check = document.createElement('input');
+    const dots = document.createElement('span');
+    const text = document.createElement('p');
+
+    check.setAttribute('type', 'checkbox');
+    check.classList.add('form-check-input', 'me-2');
+    text.textContent = task.description;
+    text.classList.add('list-items-text');
+    dots.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+    row.append(check, text, dots);
+    row.id = task.index;
+    mainList.appendChild(row);
+  });
+}
+
 const tasks = [
   {
     description: 'Excersice',
@@ -17,3 +36,5 @@ const tasks = [
     index: 3,
   },
 ];
+
+window.addEventListener('load', displayTasks(tasks));
